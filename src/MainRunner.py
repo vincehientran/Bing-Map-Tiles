bcd55d89cb0391419ee02b6af1a64e42d4c9d7b9
vincehientran <vincehientran@gmail.com>
2021-03-22 18:39:59 -0500

    finalize TileSystem class

--- src/MainRunner.py
+++ src/MainRunner.py
@@ -1,6 +1,29 @@
 from TileSystem import TileSystem as ts
 
+def run(lat1, long1, lat2, long2):
+    pixelX1, pixelY1 = ts.latlongToPixelXY(lat1, long1, 20)
+    tileX1, tileY1 = ts.pixelXYToTileXY(pixelX1, pixelY1)
+    quadKey1 = ts.tileXYToQuadKey(tileX1, tileY1, 20)
 
+    pixelX2, pixelY2 = ts.latlongToPixelXY(lat2, long2, 20)
+    tileX2, tileY2 = ts.pixelXYToTileXY(pixelX2, pixelY2)
+    quadKey2 = ts.tileXYToQuadKey(tileX2, tileY2, 20)
+
+    print(quadKey1)
+    print(tileX1, tileY1)
+
+    print(quadKey2)
+    print(tileX2, tileY2)
+
+    rowCount = tileX2 - tileX1
+    colCount = tileY2 - tileY1
+
+    quadKeys = [['' for _ in range(colCount)] for _ in range(rowCount)]
+    for i in range(len(quadKeys)):
+        for j in range(len(quadKeys[0])):
+            tileX = tileX1 + i
+            tileY = tileY1 + j
+            quadKey = ts.tileXYToQuadKey(tileX1, tileY1, 20)
 
 if __name__ == '__main__':
-    pass
+    run(41.98303725364395, -87.69917964935303, 41.97587910570054, -87.68914282321931)
--- src/TileSystem.py
+++ src/TileSystem.py
@@ -6,9 +6,32 @@ class TileSystem():
     MAX_LAT = 85.05112878
     MIN_LONG = -180
     MAX_LONG = 180
+    # http://ecn.t2.tiles.virtualearth.net/tiles/h03022223101022113021.jpeg?g=129&mkt=en-US&shading=hill&stl=H
 
     def clip(n, minValue, maxValue):
-        return math.min(math.max(n, minValue), maxValue)
+        return min(max(n, minValue), maxValue)
+
+    def mapSize(levelOfDetail):
+        return 256 << levelOfDetail
+
+    def latlongToPixelXY(lat, long, levelOfDetail):
+        lat = TileSystem.clip(lat, TileSystem.MIN_LAT, TileSystem.MAX_LAT)
+        long = TileSystem.clip(long, TileSystem.MIN_LONG, TileSystem.MAX_LONG)
+
+        x = (long + 180) / 360
+        sinLat = math.sin(lat * math.pi / 180)
+        y = 0.5 - math.log((1 + sinLat) / (1 - sinLat)) / (4 * math.pi)
+
+        size = TileSystem.mapSize(levelOfDetail)
+        pixelX = int(TileSystem.clip(x * size + 0.5, 0, size - 1))
+        pixelY = int(TileSystem.clip(y * size + 0.5, 0, size - 1))
+
+        return pixelX, pixelY
+
+    def pixelXYToTileXY(pixelX, pixely):
+        tileX = int(pixelX / 256)
+        tileY = int(pixely / 256)
+        return tileX, tileY
 
     def tileXYToQuadKey(tileX, tileY, levelOfDetail):
         quadKey = ''
